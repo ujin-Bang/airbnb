2c6e3f2b4da46cbb33dc5b31f03e734a6e5df8a2
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-09-01 16:07:02 +0900

    naver map 사용하기 -> 현재 위치로 이동하기 (좌측하단버튼) 활성화 하기 -> 위치권한 설정 -> gradle -> 'com.google.android.gms:play-services-location:20.0.0' ->로케이션 퍼미션 리졸트 -> resultCode 권한사용 / 줌/시작지점 / 현재위치

--- app/src/main/java/com/modoohealing/airbnb/MainActivity.kt
+++ app/src/main/java/com/modoohealing/airbnb/MainActivity.kt
@@ -2,13 +2,14 @@ package com.modoohealing.airbnb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.naver.maps.map.MapView
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.util.FusedLocationSource
 
 class MainActivity : AppCompatActivity(),OnMapReadyCallback {
 
     private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
     private val mapView: MapView by lazy {
         findViewById(R.id.mapView)
     }
@@ -25,6 +26,42 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback {
     override fun onMapReady(map: NaverMap) {
 
         naverMap = map
+
+        naverMap.maxZoom = 19.0 //naverMap 확대크기
+        naverMap.minZoom = 10.0 //naverMap 최소크기
+
+        //시작 위치는 시청으로 되어있음 => 강남역으로 시작위치 변경하기
+        val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.498095,127.027610))
+        naverMap.moveCamera(cameraUpdate)
+
+        //현재위치 확인버튼 => 위치를 확인해야 하므로 권한필요
+        val uiSetting = naverMap.uiSettings
+        uiSetting.isLocationButtonEnabled = true
+
+        //위치권한 받기 build.gradle => location implement추가 후
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        naverMap.locationSource = locationSource
+    }
+
+    //위치 권한 요청결과
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE){
+            return
+        }
+
+        if (locationSource.onRequestPermissionsResult(requestCode,permissions,grantResults)){
+            if (!locationSource.isActivated){
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+
     }
 
     override fun onStart() {
@@ -61,5 +98,9 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback {
     override fun onLowMemory() {
         super.onLowMemory()
         mapView.onLowMemory()
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
