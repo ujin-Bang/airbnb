d15835338b2c3e23e45270a3c26afc24e6dd7fcc
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-09-01 14:01:46 +0900

    naver map 띄우기 -> 3가지 에러 잡기 1.2 =>manifast => <application  tools:replace="android:appComponetFactory"  android:appComponentFactory="whateverString">....</application> 3번째=>gradle.properties에 android.enableJetifier=true 추가 총 3가지 추가

--- app/src/main/java/com/modoohealing/airbnb/MainActivity.kt
+++ app/src/main/java/com/modoohealing/airbnb/MainActivity.kt
@@ -2,10 +2,13 @@ package com.modoohealing.airbnb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.map.NaverMap
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
