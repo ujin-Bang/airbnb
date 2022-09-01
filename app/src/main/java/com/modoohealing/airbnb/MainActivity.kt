656f68e5bb4b7390c481f37cec30706261f7d232
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-09-01 14:22:26 +0900

    map이 백그라운드에 갔을때나 메모리가 부족했을 경우 등 제대로 작동이 되지 않을 수 있다 => fragment에서 사용하지 않을 시 activity화면에서 수명주기를 지정해 주어야 한다.

--- app/src/main/java/com/modoohealing/airbnb/MainActivity.kt
+++ app/src/main/java/com/modoohealing/airbnb/MainActivity.kt
@@ -2,13 +2,55 @@ package com.modoohealing.airbnb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 
 class MainActivity : AppCompatActivity() {
+
+    private val mapView: MapView by lazy {
+        findViewById(R.id.mapView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mapView.onCreate(savedInstanceState)
 
+    }
 
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
